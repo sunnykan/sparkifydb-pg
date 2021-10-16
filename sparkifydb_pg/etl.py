77ac6eed3df4f9963d843a9ef3106de115dee121
K S <8283247+sunnykan@users.noreply.github.com>
2021-10-16 09:42:53 -0400

    Complete function process_song_file

--- sparkifydb_pg/etl.py
+++ sparkifydb_pg/etl.py
@@ -1,90 +1,109 @@
 import os
 import glob
 import psycopg2
-import pandas as pd
+from psycopg2.extras import execute_values
+import json
 from sql_queries import *
+from dotenv import dotenv_values
+from pathlib import Path
 
 
-def process_song_file(cur, filepath):
-    # open song file
-    df = 
+def process_song_file(cur, all_files):
 
-    # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
-    
-    # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+    song_data = []
+    artist_data = []
+    for f in all_files:
+        with open(f, "r") as fhand:
+            record = json.load(fhand)
+            song_data.append(
+                (
+                    record["song_id"],
+                    record["title"],
+                    record["artist_id"],
+                    record["year"],
+                    record["duration"],
+                )
+            )
+            artist_data.append(
+                (
+                    record["artist_id"],
+                    record["artist_name"],
+                    record["artist_location"],
+                    record["artist_latitude"],
+                    record["artist_longitude"],
+                )
+            )
+
+    # insert song and artist data
+    execute_values(cur, song_table_insert, song_data)
+    execute_values(cur, artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    pass
     # open log file
-    df = 
+    # df =
 
-    # filter by NextSong action
-    df = 
+    # # filter by NextSong action
+    # df =
 
-    # convert timestamp column to datetime
-    t = 
-    
-    # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    # # convert timestamp column to datetime
+    # t =
 
-    for i, row in time_df.iterrows():
-        cur.execute(time_table_insert, list(row))
+    # # insert time data records
+    # time_data =
+    # column_labels =
+    # time_df =
 
-    # load user table
-    user_df = 
+    # for i, row in time_df.iterrows():
+    #     cur.execute(time_table_insert, list(row))
 
-    # insert user records
-    for i, row in user_df.iterrows():
-        cur.execute(user_table_insert, row)
+    # # load user table
+    # user_df =
 
-    # insert songplay records
-    for index, row in df.iterrows():
-        
-        # get songid and artistid from song and artist tables
-        cur.execute(song_select, (row.song, row.artist, row.length))
-        results = cur.fetchone()
-        
-        if results:
-            songid, artistid = results
-        else:
-            songid, artistid = None, None
+    # # insert user records
+    # for i, row in user_df.iterrows():
+    #     cur.execute(user_table_insert, row)
 
-        # insert songplay record
-        songplay_data = 
-        cur.execute(songplay_table_insert, songplay_data)
+    # # insert songplay records
+    # for index, row in df.iterrows():
+
+    #     # get songid and artistid from song and artist tables
+    #     cur.execute(song_select, (row.song, row.artist, row.length))
+    #     results = cur.fetchone()
+
+    #     if results:
+    #         songid, artistid = results
+    #     else:
+    #         songid, artistid = None, None
+
+    #     # insert songplay record
+    #     songplay_data =
+    #     cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
-    all_files = []
-    for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
-            all_files.append(os.path.abspath(f))
+    all_files = list(filepath.rglob("*.json"))
 
     # get total number of files found
     num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
+    print("{} files found in {}".format(num_files, filepath))
 
-    # iterate over files and process
-    for i, datafile in enumerate(all_files, 1):
-        func(cur, datafile)
-        conn.commit()
-        print('{}/{} files processed.'.format(i, num_files))
+    func(cur, all_files)
+    conn.commit()
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    config = dotenv_values(".env")
+    param_string = f"host={config['HOST']} dbname=sparkifydb user={config['USER']} password={config['PASSWORD']}"
+    conn = psycopg2.connect(param_string)
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(
+        cur, conn, filepath=Path.cwd() / "data" / "song_data", func=process_song_file
+    )
+    # process_data(cur, conn, filepath="data/log_data", func=process_log_file)
 
     conn.close()
 
--- sparkifydb_pg/sql_queries.py
+++ sparkifydb_pg/sql_queries.py
@@ -1,44 +1,99 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "drop table if exists songplays"
+user_table_drop = "drop table if exists users"
+song_table_drop = "drop table if exists songs"
+artist_table_drop = "drop table if exists artists"
+time_table_drop = "drop table if exists time"
 
 # CREATE TABLES
-
-songplay_table_create = """
+songplay_table_create = """create table if not exists songplays 
+(
+    songplay_id serial, 
+    user_id integer, 
+    song_id text, 
+    artist_id text, 
+    session_id integer,
+    start_time integer,
+    level text,  
+    location text, 
+    user_agent text,
+    unique(user_id),
+    unique(song_id),
+    unique(artist_id),
+    unique(user_id, session_id),
+    constraint songplays_pkey primary key (songplay_id)
+    
+);
 """
 
-user_table_create = """
+user_table_create = """create table if not exists users
+(
+    user_id integer, 
+    first_name text, 
+    last_name text, 
+    gender varchar(1), 
+    level text,
+    constraint users_pkey primary key (user_id)
+);
 """
 
-song_table_create = """
+song_table_create = """create table if not exists songs
+(
+    song_id text,
+    artist_id text,
+    title text,
+    year smallint,
+    duration integer,
+    constraint songs_pkey primary key (song_id)
+);
 """
 
-artist_table_create = """
+artist_table_create = """create table if not exists artists
+(
+    artist_id text, 
+    name text, 
+    location text, 
+    latitude integer, 
+    longitude integer,
+    constraint artists_pkey primary key (artist_id)
+);
 """
 
-time_table_create = """
+time_table_create = """create table if not exists time
+(
+    start_time integer, 
+    hour smallint, 
+    day smallint, 
+    week smallint, 
+    month smallint, 
+    year smallint, 
+    weekday smallint,
+    constraint time_pkey primary key (start_time)
+);
 """
 
 # INSERT RECORDS
 
-songplay_table_insert = """
+songplay_table_insert = """insert into songplays 
+(songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) values %s
 """
 
-user_table_insert = """
+user_table_insert = """insert into users
+(user_id, first_name, last_name, gender, level) values %s
 """
 
-song_table_insert = """
+song_table_insert = """insert into songs
+(song_id, title, artist_id, year, duration) values %s 
+on conflict on constraint songs_pkey do nothing
 """
 
-artist_table_insert = """
+artist_table_insert = """insert into artists
+(artist_id, name, location, latitude, longitude) values %s
 """
 
-
-time_table_insert = """
+time_table_insert = """insert into time
+(start_time, hour, day, week, month, year, weekday) values %s
 """
 
 # FIND SONGS
@@ -62,4 +117,3 @@ drop_table_queries = [
     artist_table_drop,
     time_table_drop,
 ]
-
